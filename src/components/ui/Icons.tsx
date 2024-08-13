8ec3e6cf497fcafd282ca160c54f65d86df03df8
jasook <gheddong2@naver.com>
2024-08-13 14:48:45 +0900

    Modify: 아이콘 컴포넌트 내 드롭다운 컴포넌트에 쓰이는 CaretDown 추가

--- src/components/ui/Icons.tsx
+++ src/components/ui/Icons.tsx
@@ -152,7 +152,6 @@ export function IconExternalLink({
   );
 }
 
-
 export function IconFolder({
   className,
   ...props
@@ -352,6 +351,25 @@ export function IconOnWait({
         stroke-linecap="round"
         stroke-linejoin="round"
       />
+    </svg>
+  );
+}
+
+export function IconCaretDown({
+  className,
+  ...props
+}: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      width="17"
+      height="9"
+      viewBox="0 0 17 9"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={cn("fill-gray-dark", className)}
+      {...props}
+    >
+      <path d="M7.81225 8.34845L0.821839 1.72595C0.165583 1.10424 0.605592 0 1.50958 0L15.4904 0C16.3944 0 16.8344 1.10424 16.1782 1.72595L9.18774 8.34845C8.80205 8.71385 8.19795 8.71385 7.81225 8.34845Z" />
     </svg>
   );
 }
