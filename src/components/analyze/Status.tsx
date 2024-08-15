f3a7368b22c6ffa5c0605610d7392d146f63ab84
jasook <gheddong2@naver.com>
2024-08-15 21:53:54 +0900

    Modify: separate by status

--- src/components/analyze/Status.tsx
+++ src/components/analyze/Status.tsx
@@ -1,29 +1,74 @@
 import { cn } from "@/lib/utils";
 import { IconClose, IconTriangle, IconCircle } from "../ui/Icons";
 
-export default function Status() {
+function Status({
+  className,
+  children,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
       className={cn(
         "flex-center-center h-[4.063rem] w-[15.375rem] gap-x-5 rounded-lg border border-line-default px-5 py-5 text-[1.268rem] font-medium -tracking-[0.01rem]",
+        className,
       )}
+      {...props}
     >
-      <div className="inline-flex items-center gap-x-2">
-        <IconClose
-          width={28}
-          height={28}
-          className="fill-accent-red stroke-accent-red stroke-[0.013rem]"
-        />
-        <span>12</span>
-      </div>
-      <div className="inline-flex items-center gap-x-2">
-        <IconTriangle />
-        <span>13</span>
-      </div>
-      <div className="inline-flex items-center gap-x-2">
-        <IconCircle />
-        <span>12</span>
-      </div>
+      {children}
     </div>
   );
 }
+
+function StatusError({
+  className,
+  children,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("inline-flex items-center gap-x-[0.125rem]", className)}
+      {...props}
+    >
+      <IconClose
+        width={28}
+        height={28}
+        className="fill-accent-red stroke-accent-red stroke-[0.013rem]"
+      />
+      <span>{children}</span>
+    </div>
+  );
+}
+
+function StatusWarning({
+  className,
+  children,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("inline-flex items-center gap-x-2", className)}
+      {...props}
+    >
+      <IconTriangle />
+      <span>{children}</span>
+    </div>
+  );
+}
+
+function StatusSuccess({
+  className,
+  children,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("inline-flex items-center gap-x-2", className)}
+      {...props}
+    >
+      <IconCircle />
+      <span>{children}</span>
+    </div>
+  );
+}
+
+export { Status, StatusError, StatusWarning, StatusSuccess };
