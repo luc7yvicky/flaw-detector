1d9f8b474dc466517e37e865a326689657bb8a76
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2024-08-16 18:05:40 +0900

    Feat : 코드분석 페이지 그리드 레이아웃 정의

--- src/app/(detect)/ai-analyze/page.tsx
+++ src/app/(detect)/ai-analyze/page.tsx
@@ -1,17 +1,23 @@
+"use client";
 import CodeViewer from "@/components/analyze/CodeViewer";
 import FileList from "@/components/analyze/FileList";
+import Button from "@/components/ui/Button";
+import { DummyData } from "@/components/analyze/FileList";
 
 export default function AiAnalyze() {
   return (
     <section className="mx-auto w-full max-w-[110rem]">
       <div className="grid grid-cols-[16rem_1fr] gap-7">
-        <div>button</div>
-        <div>[progress bar]</div>
+        <Button>선택한 파일 검사</Button>
+        <div className="h-[5rem]">[progress bar]</div>
         <div>
           <div>status panel</div>
           <FileList />
         </div>
-        <CodeViewer />
+        <div className="flex gap-7 md:flex-col lg:flex-row">
+          <CodeViewer type="asIs" />
+          <CodeViewer type="toBe" />
+        </div>
       </div>
     </section>
   );
