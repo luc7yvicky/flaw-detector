1882fd9510537e2a69dfada2ad85d421ee61599d
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2024-08-17 20:01:41 +0900

    Modify : 페이지 레이아웃에 컴포넌트 추가

--- src/app/(detect)/ai-analyze/page.tsx
+++ src/app/(detect)/ai-analyze/page.tsx
@@ -1,17 +1,44 @@
 "use client";
 import CodeViewer from "@/components/analyze/CodeViewer";
 import FileList from "@/components/analyze/FileList";
+import {
+  Status,
+  StatusError,
+  StatusSuccess,
+  StatusWarning,
+} from "@/components/analyze/Status";
 import Button from "@/components/ui/Button";
-import { DummyData } from "@/components/analyze/FileList";
+import { InputChip } from "@/components/ui/InputChip";
+import ProgressBar from "@/components/ui/ProgressBar";
+import TitleBar from "@/components/ui/TitleBar";
 
 export default function AiAnalyze() {
+  const counts = {
+    error: 8,
+    warning: 12,
+    success: 23,
+  };
+
   return (
-    <section className="mx-auto w-full max-w-[110rem]">
+    <section className="px-[1rem] mx-auto w-full max-w-[110rem]">
+      <TitleBar title="sfacweb-01" />
       <div className="grid grid-cols-[16rem_1fr] gap-7">
         <Button>선택한 파일 검사</Button>
-        <div className="h-[5rem]">[progress bar]</div>
+        <div className="border-line-default rounded-lg border p-5">
+          <ProgressBar value={0.7} className="mb-5" />
+          <div className="flex gap-7">
+            <InputChip selected percentage="75%">
+              page.tsx
+            </InputChip>
+            <InputChip>page.tsx</InputChip>
+          </div>
+        </div>
         <div>
-          <div>status panel</div>
+          <Status className="mb-6">
+            <StatusError>{counts.error}</StatusError>
+            <StatusWarning>{counts.warning}</StatusWarning>
+            <StatusSuccess>{counts.success}</StatusSuccess>
+          </Status>
           <FileList />
         </div>
         <div className="flex gap-7 md:flex-col lg:flex-row">
