9a5b63230d629f4e09c79353181f7c8e759f6a8a
sol-ee <159120751+sol-ee@users.noreply.github.com>
2024-08-19 15:10:19 +0900

    Modify : 네비게이션 UI 변경

--- src/components/ui/Pagination.tsx
+++ src/components/ui/Pagination.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { IconCaretLeft } from "./Icons";
 
 export type PaginationProps = {
   currentPage: number;
@@ -23,14 +24,17 @@ const Pagination: React.FC<PaginationProps> = ({
   return (
     <div className="flex items-center gap-[0.625rem]">
       {/* 이전 페이지로 이동 */}
-      <button
-        onClick={() => onChangePage(currentPage > 1 ? currentPage - 1 : 1)}
-        className="text-center text-[1rem] font-normal leading-6 tracking-[-0.011em] text-gray-dark focus:outline-none"
-        disabled={currentPage === 1}
-      >
-        &lt;
-      </button>
-      {/* 페이지를 클릭했을 때 */}
+
+      {startPage > 1 && (
+        <button
+          onClick={() => onChangePage(currentPage > 1 ? currentPage - 1 : 1)}
+          className="text-center text-[1rem] font-normal leading-6 tracking-[-0.011em] text-gray-dark focus:outline-none"
+          disabled={currentPage === 1}
+        >
+          <IconCaretLeft className="fill-defualt" />
+        </button>
+      )}
+      {/* 페이지를 클릭했을 때  */}
       {pages.map((page) => (
         <button
           key={page}
@@ -45,15 +49,19 @@ const Pagination: React.FC<PaginationProps> = ({
         </button>
       ))}
       {/* 다음 페이지로 이동*/}
-      <button
-        onClick={() =>
-          onChangePage(currentPage < totalPages ? currentPage + 1 : totalPages)
-        }
-        className="text-center text-[1rem] font-normal leading-6 tracking-[-0.011em] focus:outline-none"
-        disabled={currentPage === totalPages}
-      >
-        &gt;
-      </button>
+      {endPage < totalPages && (
+        <button
+          onClick={() =>
+            onChangePage(
+              currentPage < totalPages ? currentPage + 1 : totalPages,
+            )
+          }
+          className="text-center text-[1rem] font-normal leading-6 tracking-[-0.011em] focus:outline-none"
+          disabled={currentPage === totalPages}
+        >
+          <IconCaretLeft className="fill-defualt rotate-180" />
+        </button>
+      )}
     </div>
   );
 };
