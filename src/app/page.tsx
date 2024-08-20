f6862ca60c7e24762ba939cd6421f5ebad897b3e
sol-ee <159120751+sol-ee@users.noreply.github.com>
2024-08-21 04:34:54 +0900

    Feat: 랜딩 페이지 Section 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Section1 from "@/components/landing/Section1";
+import Section2 from "@/components/landing/Section2";
 import CustomerService from "@/components/ui/CustomerService";
 
 export default function LandingPage() {
   return (
     <>
       <Section1 />
+      <Section2 />
       <CustomerService className="my-[6.5rem]" />
     </>
   );
