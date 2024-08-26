0e7605deec9338d22c459b8a0237326b490dfa2c
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2024-08-26 16:52:53 +0900

    Feat : getRepoList 실행

--- src/app/my/repos/page.tsx
+++ src/app/my/repos/page.tsx
@@ -1,43 +1,13 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import Repo from "@/components/my/Repo";
 import Dropdown from "@/components/ui/Dropdown";
-import { IconCaretLeft } from "@/components/ui/Icons";
+import { Suspense } from "react";
+import { getRepoLists } from "@/lib/octokit";
+import RepoList from "@/components/my/RepoList";
+import { RepoListData } from "@/types/type";
 
-const ITEMS_PER_PAGE = 12;
-const dummyRepos = [
-  { label: "label", repositoryName: "sfac-web-1", caption: "Caption" },
-  { label: "label", repositoryName: "sfac-web-2", caption: "Caption" },
-  { label: "label", repositoryName: "sfac-web-3", caption: "Caption" },
-  { label: "label", repositoryName: "sfac-web-4", caption: "Caption" },
-  { label: "label", repositoryName: "sfac-web-5", caption: "Caption" },
-  { label: "label", repositoryName: "sfac-web-6", caption: "Caption" },
-  { label: "label", repositoryName: "sfac-web-7", caption: "Caption" },
-  { label: "label", repositoryName: "sfac-web-8", caption: "Caption" },
-  { label: "label", repositoryName: "sfac-web-9", caption: "Caption" },
-  { label: "label", repositoryName: "sfac-web-10", caption: "Caption" },
-  { label: "label", repositoryName: "sfac-web-11", caption: "Caption" },
-  { label: "label", repositoryName: "sfac-web-12", caption: "Caption" },
-];
-
-export default function Repos() {
-  const [currPage, setCurrPage] = useState<number>(1);
-  const [totalPages, setTotalPages] = useState<number>(0);
-  const [repos, setRepos] = useState<any[]>([]);
-
-  useEffect(() => {
-    setRepos(
-      dummyRepos.slice(
-        (currPage - 1) * ITEMS_PER_PAGE,
-        currPage * ITEMS_PER_PAGE,
-      ),
-    );
-    setTotalPages(Math.ceil(12 / ITEMS_PER_PAGE));
-  }, [currPage]);
-
+export default async function Repos() {
+  const repos: RepoListData[] = await getRepoLists();
   return (
     <>
       <h1 className="flex-col-center-center mt-[3.5rem] gap-y-5 text-[3.75rem] leading-[1.2] -tracking-[0.01em] text-primary-500">
@@ -88,31 +58,9 @@ export default function Repos() {
             </div>
           </div>
 
-          <div className="flex-between-center relative grid grid-cols-4 gap-x-6 gap-y-12">
-            {repos.map((repo, index) => (
-              <Repo key={index} {...repo} />
-            ))}
-
-            {/* 임시, Button */}
-            {currPage > 1 && (
-              <button
-                className="flex-center-center absolute -left-6 bottom-[47%] h-[3.25rem] w-[3.25rem] rounded-[50%] border border-gray-dark bg-white"
-                onClick={() => setCurrPage((prev) => Math.max(prev - 1, 0))}
-              >
-                <IconCaretLeft className="fill-#343330" />
-              </button>
-            )}
-            {currPage < totalPages - 1 && (
-              <button
-                className="flex-center-center absolute -right-6 bottom-[47%] h-[3.25rem] w-[3.25rem] rounded-[50%] border border-gray-dark bg-white"
-                onClick={() =>
-                  setCurrPage((prev) => Math.min(prev + 1, totalPages - 1))
-                }
-              >
-                <IconCaretLeft className="fill-#343330 rotate-180" />
-              </button>
-            )}
-          </div>
+          <Suspense fallback={<div>Loading repos...</div>}>
+            <RepoList initialRepos={repos} />
+          </Suspense>
         </section>
       </div>
     </>
