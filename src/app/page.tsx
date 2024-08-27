9fa56efac7b56bdb91c5ba4c8d7bfb46ad7e6ddd
jasook <gheddong2@naver.com>
2024-08-27 17:53:02 +0900

    Modify: 랜딩 페이지에서 빠뜨린 redirect 다시 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import LandingFeatureSection from "@/components/landing/LandingFeatureSection";
 import LandingDemoSection from "@/components/landing/LandingDemoSection";
 import LandingServiceSection from "@/components/landing/LandingServiceSection";
 import CustomerService from "@/components/ui/CustomerService";
+import { redirectIfLoggedIn } from "@/lib/redirect";
 
 export default async function LandingPage() {
+  await redirectIfLoggedIn("/my/repos");
   return (
     <>
       <LandingHeroSection />
