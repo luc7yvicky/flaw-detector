b04ae88a38089d0acd8470711ffa20cedc99b761
timetam <timetam24@naver.com>
2024-08-27 16:56:08 +0900

    Modify : pagination 관련 코드 상수화 처리

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -2,3 +2,6 @@ export const LLAMA_AUTH_URL = process.env.NEXT_PUBLIC_LLAMA_AUTH_URL;
 export const LLAMA_API_URL = process.env.NEXT_PUBLIC_LLAMA_API_URL;
 export const LLAMA_USERNAME = process.env.NEXT_PUBLIC_LLAMA_USERNAME;
 export const LLAMA_PASSWORD = process.env.NEXT_PUBLIC_LLAMA_PASSWORD;
+
+export const ITEMS_PER_DB_PAGE = 5;
+export const DB_PAGES_PER_GROUP = 10;
