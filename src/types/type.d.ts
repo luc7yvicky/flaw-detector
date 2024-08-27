a20b84b2d0d890c0458c5af47e9d50dad95b7b57
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2024-08-27 18:12:54 +0900

    Feat : 레포 공용 타입 설정

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -22,3 +22,15 @@ export type RepoListData = {
   detectedAt?: string;
   filename?: string;
 };
+
+export type RepoItem = {
+  name: string;
+  path: string;
+  type: "file" | "dir";
+  status?: "done" | "onProgress" | "onWait" | "error";
+  expanded?: boolean;
+  size?: number;
+  sha?: string;
+  loaded?: boolean;
+  items?: RepoItem[];
+};
