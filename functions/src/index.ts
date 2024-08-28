ace3aa84abe55d768d334d43ddf5a557d45883d6
timetam <timetam24@naver.com>
2024-08-29 00:09:15 +0900

    Feat : posts 컬렉션 문서 조회하는 스케줄러 함수 작성

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,42 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import { applicationDefault, initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import * as logger from "firebase-functions/logger";
+import { onSchedule } from "firebase-functions/v2/scheduler";
+
+// Firebase Admin SDK 초기화
+initializeApp({
+  credential: applicationDefault(),
+});
+const db = getFirestore();
+
+// 5분마다 실행: "every 5 minutes"
+// cron schedule expressions => https://crontab.guru/
+const SHEDULE_EXPRESSION = "0 21 * * *"; // 매일 06:00(KST)에 실행: "0 21 * * *" (한국 시간 기준) - 임시 설정
+
+const logDocuments = (snapshot: FirebaseFirestore.QuerySnapshot) => {
+  snapshot.forEach((doc) => {
+    logger.info(
+      `Document in 'posts' collection found: ${doc.id} => ${JSON.stringify(doc.data())}`,
+    );
+  });
+};
+
+export const scheduledTask = onSchedule(SHEDULE_EXPRESSION, async () => {
+  try {
+    const postsSnapshot = await db.collection("posts").get();
+
+    if (postsSnapshot.empty) {
+      logger.info("No documents found in 'posts' collection.");
+    } else {
+      logDocuments(postsSnapshot);
+    }
+  } catch (error) {
+    logger.error("Error fetching documents from Firestore:", error);
+  }
+});
