51d7732eb62f2f109f7a5a4d643550d86fd2aa05
sol-ee <159120751+sol-ee@users.noreply.github.com>
2024-08-29 02:23:57 +0900

    Test : Firebase Read/Write 테스트 코드 추가

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,14 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_MEASUREMENTID,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+} from "././src/lib/const";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
---
+++ pages/api/addDocument.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { doc, setDoc } from "firebase/firestore";
+import db from "../../firebaseConfig";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "POST") {
+    const { id, data } = req.body; // 클라이언트에서 전송된 데이터
+
+    try {
+      // Firestore에 최초 문서 추가 (또는 기존 문서 업데이트)
+      const docRef = doc(db, "사용자", id);
+      await setDoc(docRef, data);
+
+      res.status(200).json({ message: "Document successfully written!" });
+    } catch (error) {
+      console.error("Error writing document: ", error);
+      res.status(500).json({ error: "Error writing document" });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
