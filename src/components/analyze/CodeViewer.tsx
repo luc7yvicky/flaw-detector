861cac8e9f8701a91ec448f6826f5ae277800a52
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2024-09-02 17:16:58 +0900

    Feat : 선택한 파일의 코드 렌더

--- src/components/analyze/CodeViewer.tsx
+++ src/components/analyze/CodeViewer.tsx
@@ -1,34 +1,62 @@
 "use client";
-import { IconEmptyFolder, IconMagnifierWithPlus } from "../ui/Icons";
+
+import { useFileViewerStore } from "@/stores/store";
 
 import ProcessStatus from "./ProcessStatus";
+import { IconMagnifierWithPlus } from "../ui/Icons";
+import { getLanguage } from "@/lib/utils";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
-type CodeViewerProps = {
-  status?: "inProgress" | "done";
-  code?: string;
-};
+const SyntaxHighlighter = dynamic(
+  () => import("react-syntax-highlighter").then((mod) => mod.Prism),
+  { ssr: false },
+);
 
-export default function CodeViewer({ status, code }: CodeViewerProps) {
-  const renderContent = () => {
-    if (code) {
-      return (
-        <div>
-          <ProcessStatus status={status} />
-          {code}
-        </div>
-      );
-    }
+export default function CodeViewer() {
+  const { currentFile, fileContent, isLoading, error } = useFileViewerStore();
+  const [highlighterStyle, setHighlighterStyle] = useState({});
+
+  useEffect(() => {
+    import("react-syntax-highlighter/dist/esm/styles/prism/one-light").then(
+      (mod) => setHighlighterStyle(mod.default),
+    );
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!currentFile || !fileContent) {
     return (
-      <div className="flex-center-center flex-col gap-8">
+      <div className="flex-center-center flex-col gap-8 rounded-lg border border-[#c3c3c3]">
         <IconMagnifierWithPlus />
         <div className="text-2xl text-primary-500">파일을 선택하세요</div>
       </div>
     );
-  };
+  }
 
   return (
-    <div className="flex-center-center w-full flex-col rounded-lg border border-[#c3c3c3] p-11">
-      {renderContent()}
+    <div className="w-full flex-col overflow-hidden rounded-lg border border-[#c3c3c3]">
+      {/* <ProcessStatus status={status} /> */}
+      <SyntaxHighlighter
+        language={getLanguage(currentFile)}
+        style={highlighterStyle}
+        // showLineNumbers
+        wrapLines
+        className="p-11"
+        PreTag={({ children, ...props }) => (
+          <pre {...props} className="!m-0">
+            {children}
+          </pre>
+        )}
+      >
+        {fileContent}
+      </SyntaxHighlighter>
     </div>
   );
 }
