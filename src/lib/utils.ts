95df5e29a197c779e0d9250a2986482427f4f220
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2024-09-02 17:12:32 +0900

    Feat : 코드 하이라이터 언어설정을 위한 확장자 확인 함수

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from "tailwind-merge";
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+export const getLanguage = (filename: string) => {
+  const extension = filename.split(".").pop()?.toLowerCase();
+  switch (extension) {
+    case "js":
+    case "jsx":
+      return "javascript";
+    case "ts":
+    case "tsx":
+      return "typescript";
+    case "py":
+      return "python";
+    case "html":
+      return "html";
+    case "css":
+      return "css";
+    case "json":
+      return "json";
+    // 확장자-언어 추가 가능
+    default:
+      return "javascript";
+  }
+};
