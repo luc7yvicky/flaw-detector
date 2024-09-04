381b31282047be4853f9c8e5c45eef3528de715d
timetam <timetam24@naver.com>
2024-09-04 16:42:08 +0900

    Comment : Functions 폴더로 옮기기 전 임시 주석 처리

--- src/app/(posts)/vuldb/items/page.tsx
+++ src/app/(posts)/vuldb/items/page.tsx
@@ -10,20 +10,12 @@ import {
   CardTitleWrapper,
 } from "@/components/ui/Card";
 import VulDBDashboard from "@/components/vulnerability-db/VulDBDashboard";
-import {
-  VUL_DB_POSTS_API_URL,
-  WEB_CRAWLING_CERT_CC_API_URL,
-} from "@/lib/const";
+import { VUL_DB_POSTS_API_URL } from "@/lib/const";
 import { cn, formatTimestampAsDateTime } from "@/lib/utils";
-import {
-  CertCCContent,
-  CertCCLocalizedTextBlock,
-  CertCCTextBlock,
-  VulDBPost,
-} from "@/types/type";
-import { isCertCCContentType } from "@/types/typeGuards";
+import { VulDBPost } from "@/types/type";
 import Link from "next/link";
 
+// Firebase -------------------------------------------------------------------
 async function getAllVulDBPosts() {
   const res = await fetch(VUL_DB_POSTS_API_URL, {
     method: "GET",
@@ -35,22 +27,54 @@ async function getAllVulDBPosts() {
   return data;
 }
 
-async function addVulDBPost(post: VulDBPost) {
-  const res = await fetch(VUL_DB_POSTS_API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ post }),
-  });
+// async function addVulDBPost(post: VulDBPost) {
+//   const res = await fetch(VUL_DB_POSTS_API_URL, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ post }),
+//   });
 
-  if (!res.ok) {
-    throw new Error("post 추가하기 실패");
-  }
+//   if (!res.ok) {
+//     throw new Error("post 추가하기 실패");
+//   }
 
-  const data = await res.json();
-  return data;
-}
+//   const data = await res.json();
+//   return data;
+// }
+
+// Web Crawling ---------------------------------------------------------------
+// async function getCertCCWebScrawlingData() {
+//   const res = await fetch(WEB_CRAWLING_CERT_CC_API_URL, {
+//     method: "GET",
+//   });
+//   if (!res.ok) {
+//     console.log("res", res);
+//   }
+//   const data = await res.json();
+//   return data;
+// }
+
+// Translation ----------------------------------------------------------------
+// async function getGeneratedText(user_message: string) {
+//   const LOCAL_LLAMA_API_URL = "http://localhost:3000/api/generate";
+//   const res = await fetch(LOCAL_LLAMA_API_URL, {
+//     method: "POST",
+//     body: JSON.stringify({
+//       user_message,
+//       temperature: 0.9,
+//       top_p: 0.9,
+//     }),
+//   });
+
+//   if (!res.ok) {
+//     throw new Error(`생성 실패 (status code: ${res.status})`);
+//   }
+
+//   const data = await res.json();
+//   return data;
+// }
 
 function VulDBImageCardContainer({ posts }: { posts: VulDBPost[] }) {
   const newThreePosts = posts.slice(0, 3); // 최신에 수집된 3개 게시글 가져오기 (예정)
@@ -130,206 +154,89 @@ function VulDBImageCardContainer({ posts }: { posts: VulDBPost[] }) {
   );
 }
 
-async function getGeneratedText(user_message: string) {
-  const res = await fetch("http://localhost:3000/api/generate", {
-    method: "POST",
-    body: JSON.stringify({
-      user_message,
-      temperature: 0.9,
-      top_p: 0.9,
-    }),
-  });
-
-  if (!res.ok) {
-    throw new Error(`생성 실패 (status code: ${res.status})`);
-  }
-
-  const data = await res.json();
-  return data;
-}
-
-async function getCertCCWebScrawlingData() {
-  const res = await fetch(WEB_CRAWLING_CERT_CC_API_URL, {
-    method: "GET",
-  });
-  if (!res.ok) {
-    console.log("res", res);
-  }
-  const data = await res.json();
-  return data;
-}
-
 export default async function VulDBPage() {
   const posts = await getAllVulDBPosts();
-  const certCCData = await getCertCCWebScrawlingData();
+  // const certCCData = await getCertCCWebScrawlingData();
 
-  if (!posts || !certCCData) {
-    return <div>Error loading data</div>;
-  }
+  // if (!posts || !certCCData) {
+  //   return <div>Error loading data</div>;
+  // }
 
   // console.log("certCCData", certCCData);
 
-  const translateText = async (originalText: string) => {
-    const res = await getGeneratedText(
-      `다음 텍스트를 번역해주세요. CVE 번호와 url등은 제외하고 이어서 번역해주세요. ${originalText}`,
-    );
-    return res.generated_text;
-  };
+  // const translateText = async (originalText: string) => {
+  //   const res = await getGeneratedText(
+  //     `다음 텍스트를 번역해주세요. CVE 번호와 url등은 제외하고 이어서 번역해주세요. ${originalText}`,
+  //   );
+  //   return res.generated_text;
+  // };
 
-  const translateContent = async (content: VulDBPost["content"]) => {
-    const translateSection = async (section: CertCCLocalizedTextBlock) => {
-      return Promise.all(
-        section.original.map(async (item: CertCCTextBlock) => ({
-          ...item,
-          text: await translateText(item.text),
-        })),
-      );
-    };
+  // const translateSection = async (section: CertCCLocalizedTextBlock) => {
+  //   if (!section || !section.original || section.original.length === 0) {
+  //     return section ? { original: [], translated: [] } : null;
+  //   }
 
-    if (!isCertCCContentType(content)) {
-      return;
-    }
+  //   return Promise.all(
+  //     section.original.map(async (item: CertCCTextBlock) => ({
+  //       ...item,
+  //       text: await translateText(item.text),
+  //     })),
+  //   );
+  // };
 
-    return {
-      overview: {
-        original: content.overview.original,
-        translated: await translateSection(content.overview),
-      },
-      description: {
-        original: content.description.original,
-        translated: await translateSection(content.description),
-      },
-      impact: {
-        original: content.impact.original,
-        translated: await translateSection(content.impact),
-      },
-      solution: {
-        original: content.solution.original,
-        translated: await translateSection(content.solution),
-      },
-      cveIDs: content.cveIDs,
-    };
-  };
+  // const translateContent = async (content: VulDBPost["content"]) => {
+  //   if (!isCertCCContentType(content)) {
+  //     return;
+  //   }
 
-  const translatedPosts = await Promise.all(
-    certCCData.posts.map(async (post: VulDBPost) => {
-      const originalTitle = post.title.original;
-      const translatedTitle = await translateText(originalTitle);
-      const translatedContent = await translateContent(post.content);
+  //   return {
+  //     overview: {
+  //       original: content.overview.original,
+  //       translated: await translateSection(content.overview),
+  //     },
+  //     description: {
+  //       original: content.description.original,
+  //       translated: await translateSection(content.description),
+  //     },
+  //     impact: {
+  //       original: content.impact.original,
+  //       translated: await translateSection(content.impact),
+  //     },
+  //     solution: {
+  //       original: content.solution.original,
+  //       translated: await translateSection(content.solution),
+  //     },
+  //     cveIDs: content.cveIDs,
+  //   };
+  // };
 
-      return {
-        ...post,
-        title: { original: originalTitle, translated: translatedTitle },
-        content: translatedContent,
-      };
-    }),
-  );
+  // const translatedPosts = await Promise.all(
+  //   certCCData.posts.map(async (post: VulDBPost) => {
+  //     const originalTitle = post.title.original;
 
-  console.log(posts);
+  //     const translatedTitle = await translateText(originalTitle);
+  //     const translatedContent = await translateContent(post.content);
 
-  if (translatedPosts) {
-    translatedPosts.map(async (post: VulDBPost) => {
-      return await addVulDBPost(post);
-    });
-  }
+  //     return {
+  //       ...post,
+  //       title: { original: originalTitle, translated: translatedTitle },
+  //       content: translatedContent,
+  //     };
+  //   }),
+  // );
+
+  // console.log(translatedPosts);
+
+  // if (translatedPosts) {
+  //   translatedPosts.map(async (post: VulDBPost) => {
+  //     return await addVulDBPost(post);
+  //   });
+  // }
 
   return (
     <div className="mx-auto mb-[1.188rem] mt-[1.688rem] flex w-[82.063rem] flex-col gap-[4.75rem]">
       <VulDBImageCardContainer posts={posts.data} />
       <VulDBDashboard posts={posts.data} /> {/* 취약점 DB & 실시간 Topic */}
-      {/* {posts.data.map((post: VulDBPost) => {
-        return (
-          <div key={post.id} className="rounded-md border border-slate-400 p-6">
-            <h2 className="text-2xl font-bold">
-              {post.title.translated} ({post.source})
-            </h2>
-            <Link
-              href={post.page_url}
-              className="float-right underline underline-offset-2"
-              target="_blank"
-            >
-              Cert/CC 페이지에서 보기
-            </Link>
-            <div className="mt-10 flex flex-col gap-16 p-3">
-              {isCertCCContentType(post.content) && (
-                <>
-                  <section>
-                    <h3 className="text-xl font-semibold">Overview</h3>
-                    <ul>
-                      {post.content.overview.translated.map((item) => {
-                        return <li key={item.id}>{item.text}</li>;
-                      })}
-                      {post.content.overview.original.map((item) => {
-                        return <li key={item.id}>{item.text}</li>;
-                      })}
-                    </ul>
-                  </section>
-                  <section>
-                    <h3 className="text-xl font-semibold">Description</h3>
-                    <ul>
-                      {post.content.description.translated.map((item) => {
-                        return <li key={item.id}>{item.text}</li>;
-                      })}
-                      {post.content.description.original.map((item) => {
-                        return <li key={item.id}>{item.text}</li>;
-                      })}
-                    </ul>
-                  </section>
-                  {post.content.impact.original.length > 0 && (
-                    <section>
-                      <h3 className="text-xl font-semibold">Impact</h3>
-                      <ul>
-                        {post.content.impact.translated.map((item) => {
-                          return <li key={item.id}>{item.text}</li>;
-                        })}
-                        {post.content.impact.original.map((item) => {
-                          return <li key={item.id}>{item.text}</li>;
-                        })}
-                      </ul>
-                    </section>
-                  )}
-
-                  {post.content.solution.original.length > 0 && (
-                    <section>
-                      <h3 className="text-xl font-semibold">Solution</h3>
-                      <ul>
-                        {post.content.solution.translated.map((item) => {
-                          return <li key={item.id}>{item.text}</li>;
-                        })}
-                        {post.content.solution.original.map((item) => {
-                          return <li key={item.id}>{item.text}</li>;
-                        })}
-                      </ul>
-                    </section>
-                  )}
-
-                  {post.content.cveIDs.length > 0 && (
-                    <section className="flex items-end gap-3">
-                      <h3 className="text-xl font-semibold">CVE IDs: </h3>
-                      <ul className="flex gap-5">
-                        {post.content.cveIDs.map((item) => {
-                          return (
-                            <li
-                              key={item}
-                              className="text-red-500 underline-offset-2 hover:underline"
-                            >
-                              <Link
-                                href={`https://www.cve.org/CVERecord?id=${item}`}
-                              >
-                                {item}
-                              </Link>
-                            </li>
-                          );
-                        })}
-                      </ul>
-                    </section>
-                  )}
-                </>
-              )}
-            </div>
-          </div>
-        );
-      })} */}
     </div>
   );
 }
