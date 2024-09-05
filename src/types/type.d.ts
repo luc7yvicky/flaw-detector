9a1e6fcc2e104fdd34d5d92746386e465979242a
sol-ee <159120751+sol-ee@users.noreply.github.com>
2024-09-05 16:21:36 +0900

    Modify : 데이터 구조 변경

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -40,34 +40,49 @@ export type RepoContentItem = {
   error?: string;
 };
 
-export type CertCCTextBlock = { id: string; text: string };
+export type CertCCCTextBlock = { id: string; text: string };
 
-export type CertCCLocalizedTextBlock = {
-  original: CertCCTextBlock[];
-  translated: CertCCTextBlock[];
+export type CnnvdTextBlock = {
+  text: string;
+};
+
+export type CertCCCLocalizedTextBlock = {
+  original: TextBlock[];
+  translated: TextBlock[];
+};
+
+export type CnnvdLocalizedTextBlock = {
+  original: string;
+  translated: string;
 };
 
 // CERT/CC 게시글 내용
 export type CertCCContent = {
-  overview: CertCCLocalizedTextBlock;
-  description: CertCCLocalizedTextBlock;
-  impact: CertCCLocalizedTextBlock;
-  solution: CertCCLocalizedTextBlock;
+  overview: CertCCCLocalizedTextBlock;
+  description: CertCCCLocalizedTextBlock;
+  impact: CertCCCLocalizedTextBlock;
+  solution: CertCCCLocalizedTextBlock;
   cveIDs: string[];
 };
 
 // CNNVD 게시글 내용
-export type CnnvdContent = { block_id: string; text: string }[];
+export type CnnvdContent = {
+  description: CnnvdLocalizedTextBlock;
+  introduction: CnnvdLocalizedTextBlock;
+  vulnDetail: CnnvdLocalizedTextBlock;
+  remediation: CnnvdLocalizedTextBlock;
+};
 
 export type VulDBPost = {
   id: string;
-  label: "기타" | "취약성 보고서" | "취약성 알림" | "취약성 경고";
+  label: "기타" | "취약성 보고서" | "취약성 알림";
   source: "CERT/CC" | "CNNVD";
   page_url: string;
   title: {
     original: string;
     translated: string;
   };
+  source_created_at: { seconds: number; nanoseconds: number }; //원문 게시글 등록일
   created_at: { seconds: number; nanoseconds: number }; // firestore timestamp
   content: CertCCContent | CnnvdContent;
 };
