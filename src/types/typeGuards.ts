7fe5816d67a8074170ed036e9c3cf49cc4aa3084
sol-ee <159120751+sol-ee@users.noreply.github.com>
2024-09-05 17:49:43 +0900

    Feat : CNNVD content 타입 추가

--- src/types/typeGuards.ts
+++ src/types/typeGuards.ts
@@ -5,3 +5,14 @@ export const isCertCCContentType = (
 ): content is CertCCContent => {
   return (content as CertCCContent).overview !== undefined;
 };
+
+export const isCnnvdContentType = (
+  content: CertCCContent | CnnvdContent,
+): content is CnnvdContent => {
+  return (
+    "description" in content &&
+    typeof content.description === "object" &&
+    "original" in content.description &&
+    "translated" in content.description
+  );
+};
