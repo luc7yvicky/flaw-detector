cf4939d883c0b057d37c17cc5efd2f78ad031b15
timetam <timetam24@naver.com>
2024-09-06 13:04:42 +0900

    Design : 고객센터 ui 수정

--- src/components/ui/CustomerService.tsx
+++ src/components/ui/CustomerService.tsx
@@ -1,26 +1,28 @@
 import { cn } from "@/lib/utils";
 import { Input } from "./Input";
 import { TextArea } from "./TextArea";
+import Button from "./Button";
 
 export default function CustomerService({ className }: { className?: string }) {
   return (
     <section
       className={cn(
-        "mx-auto flex h-[46rem] w-full max-w-[82.125rem] justify-between gap-[6.875rem]",
+        "mx-auto flex h-[50.813rem] w-full max-w-[96.125rem] justify-between",
         className,
       )}
     >
-      <div className="h-full w-[25.625rem]">
-        <h2 className="text-5xl font-bold leading-[4.375rem] tracking-tighter text-primary-500">
-          서비스 이용에
+      <div className="h-full w-[28.125rem]">
+        <h2 className="text-6xl font-bold leading-[5.625rem] tracking-tighter text-primary-500">
+          서비스이용에
           <br />
           문제가 생겼나요?
         </h2>
         <p className="mt-8 text-lg font-medium leading-8 text-gray-default">
-          이용하면서 문제가 생겼다면 언제든지 문의주세요. 서비스 개발과 성장에
-          큰 도움이 됩니다.
+          이용하면서 문제가 생겼다면 언제든지 문의주세요.
+          <br />
+          서비스 개발과 성장에 큰 도움이 됩니다.
         </p>
-        <ul className="mt-[20.625rem]">
+        <ul className="mt-[23.125rem]">
           <li className="mb-8 flex flex-col gap-2">
             <span className="text-xl font-semibold leading-[1.875rem] text-gray-dark">
               Email
@@ -41,11 +43,11 @@ export default function CustomerService({ className }: { className?: string }) {
       </div>
       <form
         className={cn(
-          "flex h-full w-full max-w-[49.625rem] flex-col gap-6 rounded-[2.5rem] border border-primary-500 bg-white px-12 py-10",
+          "flex h-full w-[61.563rem] flex-col gap-8 rounded-[2.5rem] border border-primary-500 bg-white p-[3.75rem]",
         )}
       >
         <div>
-          <h3 className="mb-2 text-2xl font-bold leading-9">문의하기</h3>
+          <h3 className="mb-6 text-2xl font-bold leading-9">문의하기</h3>
           <p className="text-base font-medium tracking-[-0.011em] text-[#8F8F8F]">
             문의하고싶은 내용을 구체적으로 작성해주셔야 피드백이 정상적으로
             반영됩니다.
@@ -84,12 +86,10 @@ export default function CustomerService({ className }: { className?: string }) {
           <TextArea
             id="message"
             placeholder="내용을 적어주세요."
-            className="mt-2 h-56"
+            className="mt-2 h-52"
           />
         </div>
-        <button className="rounded-lg bg-primary-500 py-[0.813rem] text-lg font-semibold text-white">
-          문의 보내기
-        </button>
+        <Button className="py-[0.813rem] text-lg">문의 보내기</Button>
       </form>
     </section>
   );
