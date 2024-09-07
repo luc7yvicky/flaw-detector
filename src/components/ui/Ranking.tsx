131edd3eaade1451e8ce4ec9feed251853385ed9
timetam <timetam24@naver.com>
2024-09-08 02:55:13 +0900

    Design : 실시간 Topic 검색어가 없을 경우 UI 추가

--- src/components/ui/Ranking.tsx
+++ src/components/ui/Ranking.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 
 export type RankingProps = React.HTMLAttributes<HTMLUListElement> & {};
 
-const topics = [
+const dummySearchKeywords: string[] = [
   "유닛테스트",
   "웹뷰 프레임워크",
   "허프만 코딩 구현",
@@ -15,26 +15,42 @@ const topics = [
   "클린 코어",
 ];
 
+const renderTopics = (topics: string[], haveSearchKeywords: boolean) => {
+  return topics.map((topic, index) => (
+    <li
+      key={index}
+      className={cn(
+        "border-b border-line-light py-4 text-lg font-medium leading-[1.361rem] tracking-[-0.01em]",
+        index === topics.length - 1 && "border-none",
+        !haveSearchKeywords && "select-none blur-md",
+      )}
+    >
+      {index + 1}. {topic}
+    </li>
+  ));
+};
+
 export const Ranking: React.FC<RankingProps> = ({ className, ...props }) => {
+  const searchKeywords: string[] = [];
+
   return (
     <ul
       className={cn(
-        "h-[36.25rem] w-full rounded-[1.25rem] border border-[#CFCFCF] px-9 py-5",
+        "relative h-[36.25rem] w-full rounded-[1.25rem] border border-[#CFCFCF] px-9 py-5",
         className,
       )}
       {...props}
     >
-      {topics.map((topic, index) => (
-        <li
-          key={index}
-          className={cn(
-            "border-b border-line-light py-4 text-lg font-medium leading-[1.361rem] tracking-[-0.01em]",
-            index === topics.length - 1 && "border-none",
-          )}
-        >
-          {index + 1}. {topic}
-        </li>
-      ))}
+      {searchKeywords.length > 0 ? (
+        renderTopics(searchKeywords, true)
+      ) : (
+        <>
+          {renderTopics(dummySearchKeywords, false)}
+          <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform whitespace-nowrap text-xl font-medium">
+            데이터를 준비중입니다.
+          </p>
+        </>
+      )}
     </ul>
   );
 };
