04e34a67d7bffa94476dec89ad74b6a4e4d34599
timetam <timetam24@naver.com>
2024-09-08 14:30:30 +0900

    Modify : Input에 초기값이 있을 경우 배경색 채우기

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,15 +1,27 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   isErrored?: boolean;
 };
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ isErrored, className, type, ...props }, ref) => {
+  ({ value: initialValue, isErrored, className, type, ...props }, ref) => {
+    const [value, setValue] = useState(initialValue || "");
     const [isValid, setIsValid] = useState(false);
+
+    useEffect(() => {
+      if (initialValue !== undefined) {
+        setValue(initialValue);
+      }
+    }, [initialValue]);
+
+    const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newValue = e.target.value;
+      setValue(newValue);
+    };
 
     const onBlurInput = (e: React.FocusEvent<HTMLInputElement>) => {
       if (e.target.value && !isErrored) {
@@ -21,7 +33,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 
     return (
       <input
+        value={value}
         type={type}
+        onChange={onChangeInput}
         onBlur={onBlurInput}
         className={cn(
           "h-[51px] w-full rounded-lg border p-3 text-lg font-medium text-gray-dark outline-none placeholder:font-medium disabled:cursor-not-allowed disabled:bg-bggray-light",
@@ -34,6 +48,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           isErrored
             ? "border-accent-red placeholder:text-accent-red"
             : "border-line-default placeholder:text-gray-light focus:border-primary-500",
+          value && !isErrored && "bg-purple-light focus:bg-transparent",
         )}
         ref={ref}
         {...props}
