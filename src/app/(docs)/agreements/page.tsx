87ffb48695c98d78afe73ebdd790bfa80c524430
timetam <timetam24@naver.com>
2024-09-10 20:59:03 +0900

    Feat : 이용약관 및 개인정보처리방침 페이지 퍼블리싱

--- src/app/(docs)/agreements/page.tsx
+++ src/app/(docs)/agreements/page.tsx
@@ -1,21 +1,15 @@
-import { koreanLoremIpsum } from "@/lib/dummy";
-
 export default function AgreementsPage() {
   return (
-    <>
-      <div className="flex-col-center-center gap-y-[0.625rem] text-white">
-        <h1 className="text-[2rem] font-semibold leading-[2.8rem] tracking-[1.5%]">
-          우리는 당신의 개인정보 보호와 신뢰를 중요하게 생각합니다.
-        </h1>
-        <h2 className="flex-col-center-center text-xl font-medium leading-7">
-          <span>개인정보 보호 센터는 고객의 정보를 수집해서</span>
-          <span>사용하는 방법에 대한 고객의 선택 사항을 비롯하여</span>
-          <span>
-            개인정보 보호 항목에 대한 정보를 쉽게 찾을 수 있도록 해 줍니다.
-          </span>
-        </h2>
+    <div className="flex flex-col items-center pt-[28.813rem]">
+      <h1 className="text-[2rem] font-semibold leading-[2.8rem] tracking-[1.5%]">
+        플로디텍터 일반 사용 약관
+      </h1>
+      <div className="mt-3 text-center text-xl font-medium">
+        <p> 모든 사업 관계에서 귀하는 일련의 약관에 동의합니다. </p>
+        <p>본 약관은 플로디텍터 사용에 대해 당사가 귀하와 체결한 계약입니다.</p>
+
+        <time className="mt-3 block">발행일 : 2024.08.28</time>
       </div>
-      <p>{koreanLoremIpsum}</p>
-    </>
+    </div>
   );
 }
