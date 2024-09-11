d0144fc87eab3583ef35042cac82c7d0fba452c0
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2024-09-11 17:05:19 +0900

    Chore : 충돌 해결

--- src/app/(posts)/vuldb/items/[id]/page.tsx
+++ src/app/(posts)/vuldb/items/[id]/page.tsx
@@ -21,7 +21,7 @@ export default async function VulnerabilityDBDetailPage({
 
   return (
     <div className="mb-[8.596rem] mt-[2.063rem] flex flex-col items-center gap-[3.75rem] px-[1rem]">
-      <ArticleDetail post={post} />
+      <ArticleDetail post={post} userId={userId} />
       {/* <VulnerabilityGrid /> */}
       <SimilarInfoPosts posts={posts} postId={postId} userId={userId} />
     </div>
