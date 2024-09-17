3f30c2b7b98c84222285152d0fb441fde9f1a944
jasook <gheddong2@naver.com>
2024-09-17 23:37:40 +0900

    Modify: 제외하는 요소를 여러 개 받을 수 있게끔 매개변수 데이터 타입 수정

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -1,12 +1,29 @@
 import { RefObject, useEffect } from "react";
 
-const useOutsideClick = (ref: RefObject<HTMLElement>, callback: () => void) => {
+const useOutsideClick = (
+  ref: RefObject<HTMLElement>[] | RefObject<HTMLElement>,
+  callback: (event?: MouseEvent) => void,
+) => {
   useEffect(() => {
     const listener = (event: MouseEvent) => {
-      if (!ref.current || ref.current.contains(event.target as Node)) {
-        return;
+      if (Array.isArray(ref)) {
+        // refs가 여러 개일 때
+        const refs = ref as RefObject<HTMLElement>[];
+        if (
+          refs?.some(
+            (ref) => !ref.current || ref.current.contains(event.target as Node),
+          )
+        ) {
+          return;
+        }
+      } else {
+        // refs가 하나일 때
+        if (!ref.current || ref.current.contains(event.target as Node)) {
+          return;
+        }
       }
-      callback();
+
+      event ? callback(event) : callback();
     };
 
     document.addEventListener("mousedown", listener);
