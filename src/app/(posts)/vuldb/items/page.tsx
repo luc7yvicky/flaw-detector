7ab9f195ce5750ff5bc06e26baaf00cbedcfcac2
timetam <timetam24@naver.com>
2024-09-19 10:59:44 +0900

    Feat : 유효성 검사, 예외 처리, 스켈레톤 추가

--- src/app/(posts)/vuldb/items/page.tsx
+++ src/app/(posts)/vuldb/items/page.tsx
@@ -6,39 +6,47 @@ import { getUserPinnedPosts } from "@/lib/api/users";
 import { VulDBPost, VulDBPostWithChip } from "@/types/post";
 import { Timestamp } from "firebase/firestore";
 
-/** Hot chip 또는 New chip을 적용하는 함수 */
 const applyChips = (posts: VulDBPost[]): VulDBPostWithChip[] => {
   const now = new Date();
 
-  // 조회수 기준으로 상위 10개의 hot 게시글 선정
-  const hotPosts = posts
-    .filter((post) => post.views > 0)
-    .slice()
-    .sort((a, b) => b.views - a.views)
-    .slice(0, 10);
+  try {
+    // 조회수 기준으로 상위 10개의 hot 게시글 선정
+    const hotPosts = posts
+      .filter((post) => post.views && post.views > 0)
+      .slice()
+      .sort((a, b) => b.views - a.views)
+      .slice(0, 10);
 
-  return posts.map((post) => {
-    const postDate = new Timestamp(
-      post.created_at.seconds,
-      post.created_at.nanoseconds,
-    ).toDate();
-    const diffInHours = (now.getTime() - postDate.getTime()) / (1000 * 60 * 60);
+    return posts.map((post) => {
+      const postDate = post.created_at
+        ? new Timestamp(
+            post.created_at.seconds,
+            post.created_at.nanoseconds,
+          ).toDate()
+        : new Date(0);
 
-    // 48시간 이내에 생성된 게시글은 "new" chip을 받음
-    const isNew = diffInHours <= 48;
+      const diffInHours =
+        (now.getTime() - postDate.getTime()) / (1000 * 60 * 60);
 
-    // 조회수 상위 10개에 포함된 게시글은 "hot" chip을 받음
-    const isHot = hotPosts.some((hotPost) => hotPost.id === post.id);
+      // 48시간 이내에 생성된 게시글은 "new" chip을 받음
+      const isNew = diffInHours <= 48;
 
-    // new지만 hot에 포함된 경우 "hot"으로 처리
-    const chip: "hot" | "new" | "" = isHot ? "hot" : isNew ? "new" : "";
+      // 조회수 상위 10개에 포함된 게시글은 "hot" chip을 받음
+      const isHot = hotPosts.some((hotPost) => hotPost.id === post.id);
 
-    return {
-      ...post,
-      chip,
-      isScrapped: false,
-    };
-  });
+      // new지만 hot에 포함된 경우 "hot"으로 처리
+      const chip: "hot" | "new" | "" = isHot ? "hot" : isNew ? "new" : "";
+
+      return {
+        ...post,
+        chip,
+        isScrapped: false,
+      };
+    });
+  } catch (error) {
+    console.error("applyChips 함수에서 오류 발생: ", error);
+    return [];
+  }
 };
 
 export default async function VulDBPage() {
@@ -46,8 +54,6 @@ export default async function VulDBPage() {
     const posts = (await getAllPosts()) || [];
     const session = await auth();
     const userId = session?.user?.userId;
-
-    if (!userId) return null;
 
     const postsWithScrap = await getUserPinnedPosts(userId);
 
@@ -62,16 +68,14 @@ export default async function VulDBPage() {
     const postsWithChips = applyChips(posts);
     // 최신 게시물을 created_at 기준으로 정렬 + 동일한 created_at일 경우 id 기준 정렬
     const sortedPostsByDate = postsWithChips.sort((a, b) => {
-      const dateA = new Timestamp(
-        a.created_at.seconds,
-        a.created_at.nanoseconds,
-      ).toDate();
-      const dateB = new Timestamp(
-        b.created_at.seconds,
-        b.created_at.nanoseconds,
-      ).toDate();
+      const dateA = a.created_at
+        ? new Timestamp(a.created_at.seconds, a.created_at.nanoseconds).toDate()
+        : new Date(0);
 
-      // created_at이 같다면 id로 정렬
+      const dateB = b.created_at
+        ? new Timestamp(b.created_at.seconds, b.created_at.nanoseconds).toDate()
+        : new Date(0);
+
       return dateB.getTime() === dateA.getTime()
         ? a.id.localeCompare(b.id)
         : dateB.getTime() - dateA.getTime();
@@ -83,10 +87,6 @@ export default async function VulDBPage() {
       <div className="relative mx-auto mb-[1.188rem] mt-[1.688rem] flex min-h-[2445px] w-[82.063rem] flex-col gap-[4.75rem] px-[1rem]">
         <VulDBImageCardContainer posts={top3RecentPosts} />
         <Search initialPosts={postsWithChips} postsWithScrap={postsWithScrap} />
-        <div className="flex justify-between">
-          {/* <VulDBDashboard posts={sortedPostsByDate} /> */}
-          {/* <RealTimeTopic /> */}
-        </div>
       </div>
     );
   } catch (error) {
