4ff4ce0f209495fdcd969720b8dca1df04732a24
timetam <timetam24@naver.com>
2024-10-16 15:06:25 +0900

    Refactor : 취약점 DB 컴포넌트 구조 개선

--- src/app/(posts)/vuldb/items/page.tsx
+++ src/app/(posts)/vuldb/items/page.tsx
@@ -3,16 +3,16 @@
 import ExceptionHandlingMessage from "@/components/vulnerability-db/ExceptionHandlingMessage";
 import RealTimeTopic from "@/components/vulnerability-db/RealTimeTopic";
 import Search from "@/components/vulnerability-db/Search";
+import VulDBDashboard from "@/components/vulnerability-db/VulDBDashboard";
 import VulDBImageCardContainer from "@/components/vulnerability-db/VulDBImageCardContainer";
-import VulDBNoPosts from "@/components/vulnerability-db/VulDBNoPosts";
 import {
   VulDBDashboardSkeleton,
   VulDBImageCardContainerSkeleton,
 } from "@/components/vulnerability-db/VulDBSkeleton";
+import { useSessionStore } from "@/context/SessionProvider";
 import { ITEMS_PER_DB_PAGE } from "@/lib/const";
 import { useVulDBPosts } from "@/lib/queries/useVulDBPosts";
 import { useState } from "react";
-import { useSessionStore } from "@/context/SessionProvider";
 
 export default function VulDBPage() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -30,36 +30,31 @@ export default function VulDBPage() {
     );
 
   try {
-    if (postsLoading) {
-      return (
-        <div className="relative mx-auto mt-[1.688rem] flex min-h-[147rem] w-full max-w-[82.063rem] flex-col gap-[4.75rem] overflow-hidden px-[1rem]">
-          <VulDBImageCardContainerSkeleton />
-          <div className="mt-[4.75rem] grid grid-cols-[1fr_22rem] gap-5">
-            <VulDBDashboardSkeleton />
-            <RealTimeTopic />
-          </div>
-        </div>
-      );
-    }
-
-    // if (posts.length === 0 && userId && selectedChip === "") {
-    //   return <VulDBNoPosts />;
-    // }
-
     return (
       <div className="relative mx-auto mt-[1.688rem] flex min-h-[147rem] w-full max-w-[82.063rem] flex-col gap-[4.75rem] overflow-hidden px-[1rem]">
-        <VulDBImageCardContainer posts={latestPosts} />
-        <Search
-          posts={posts}
-          currentPage={currentPage}
-          totalPages={totalPages}
-          setCurrentPage={setCurrentPage}
-          setSelectedChip={setSelectedChip}
-          selectedChip={selectedChip}
-          userId={userId}
-          prefetchPage={prefetchPage}
-          setSearchTerm={setSearchTerm}
-        />
+        {postsLoading ? (
+          <VulDBImageCardContainerSkeleton />
+        ) : (
+          <VulDBImageCardContainer posts={latestPosts} />
+        )}
+        <Search setCurrentPage={setCurrentPage} setSearchTerm={setSearchTerm} />
+        <div className="flex justify-between">
+          {postsLoading ? (
+            <VulDBDashboardSkeleton />
+          ) : (
+            <VulDBDashboard
+              posts={posts}
+              currentPage={currentPage}
+              totalPages={totalPages}
+              setCurrentPage={setCurrentPage}
+              setSelectedChip={setSelectedChip}
+              selectedChip={selectedChip}
+              userId={userId}
+              prefetchPage={prefetchPage}
+            />
+          )}
+          <RealTimeTopic />
+        </div>
       </div>
     );
   } catch (error) {
