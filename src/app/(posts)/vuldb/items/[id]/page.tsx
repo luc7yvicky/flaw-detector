aceae6f4acc12aed3bbba2ae78d46e89cccc1eaa
timetam <timetam24@naver.com>
2024-11-26 14:35:29 +0900

    Refactor : react-toastify 관련 코드 하나의 파일로 통합

--- src/app/(posts)/vuldb/items/[id]/page.tsx
+++ src/app/(posts)/vuldb/items/[id]/page.tsx
@@ -3,6 +3,13 @@ import { Floating } from "@/components/ui/Floating";
 import ArticleDetail from "@/components/vulnerability-db/ArticleDetail";
 import SimilarInfoPosts from "@/components/vulnerability-db/SimilarInfoPosts";
 import { redirectIfNotLoggedIn } from "@/lib/redirect";
+import dynamic from "next/dynamic";
+
+const ToastContainer = dynamic(() =>
+  import("@/components/vulnerability-db/Toast").then(
+    (mod) => mod.ToastContainer,
+  ),
+);
 
 export default async function VulnerabilityDBDetailPage({
   params,
@@ -30,6 +37,7 @@ export default async function VulnerabilityDBDetailPage({
     <div className="relative mx-auto mb-[8.596rem] mt-[2.063rem] flex w-full max-w-[120rem] flex-col items-center gap-[3.75rem] overflow-hidden px-[1rem]">
       <ArticleDetail userId={userId} postId={postId} />
       <SimilarInfoPosts postId={postId} userId={userId} />
+      <ToastContainer />
       <div className="width-[4.75rem] absolute right-[8.75rem] top-[46.313rem]">
         <Floating variant="chat" className="fixed top-[46.313rem]" />
       </div>
