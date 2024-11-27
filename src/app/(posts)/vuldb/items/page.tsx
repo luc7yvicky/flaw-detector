d4789b8540509b29eda95009f5746f17d772587b
timetam <timetam24@naver.com>
2024-11-27 21:14:06 +0900

    Refactor : VulDB 메인 페이지 컴포넌트 구조변경

--- src/app/(posts)/vuldb/items/page.tsx
+++ src/app/(posts)/vuldb/items/page.tsx
@@ -1,18 +1,7 @@
 import { auth } from "@/auth";
-import RealTimeTopic from "@/components/vulnerability-db/RealTimeTopic";
-import Search from "@/components/vulnerability-db/Search";
-import VulDBDashboard from "@/components/vulnerability-db/VulDBDashboard";
+import VulDBMainContent from "@/components/vulnerability-db/VulDBMainContent";
 import VulDBImageCardContainer from "@/components/vulnerability-db/VulDBImageCardContainer";
-import {
-  VulDBDashboardSkeleton,
-  VulDBImageCardContainerSkeleton,
-} from "@/components/vulnerability-db/VulDBSkeleton";
-import { useSessionStore } from "@/context/SessionProvider";
-
-import { BASE_URL, ITEMS_PER_DB_PAGE } from "@/lib/const";
-// import { useVulDBPosts } from "@/lib/queries/useVulDBPosts";
-// import { useState } from "react";
-import { Timestamp } from "firebase/firestore";
+import { BASE_URL } from "@/lib/const";
 
 async function fetchLatestPosts() {
   const res = await fetch(`${BASE_URL}/api/posts/latest`);
@@ -26,47 +15,10 @@ export default async function VulDBPage() {
   const session = await auth();
   const userId = session?.user?.userId || null;
 
-  // const [currentPage, setCurrentPage] = useState(1);
-  // const [selectedChip, setSelectedChip] = useState<"hot" | "new" | "all">(
-  //   "all",
-  // );
-  // const [searchTerm, setSearchTerm] = useState<string[]>([]);
-
-  // const { posts, totalPages, postsLoading, latestPosts, prefetchPage } =
-  //   useVulDBPosts(
-  //     userId,
-  //     currentPage,
-  //     ITEMS_PER_DB_PAGE,
-  //     selectedChip,
-  //     searchTerm,
-  //   );
-
   return (
     <div className="relative mx-auto mt-[1.688rem] flex min-h-[147rem] w-full max-w-[82.063rem] flex-col gap-[4.75rem] overflow-hidden px-[1rem]">
       <VulDBImageCardContainer posts={latestPosts} />
-      {/* {postsLoading ? (
-        <VulDBImageCardContainerSkeleton />
-      ) : (
-        <VulDBImageCardContainer posts={latestPosts} />
-      )}
-      <Search setCurrentPage={setCurrentPage} setSearchTerm={setSearchTerm} />
-      <div className="grid grid-cols-[1fr_22rem] gap-5">
-        {postsLoading ? (
-          <VulDBDashboardSkeleton />
-        ) : (
-          <VulDBDashboard
-            posts={posts}
-            currentPage={currentPage}
-            totalPages={totalPages}
-            setCurrentPage={setCurrentPage}
-            setSelectedChip={setSelectedChip}
-            selectedChip={selectedChip}
-            userId={userId}
-            prefetchPage={prefetchPage}
-          />
-        )}
-        <RealTimeTopic />
-      </div> */}
+      <VulDBMainContent userId={userId} />
     </div>
   );
 }
