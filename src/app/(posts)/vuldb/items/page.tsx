e7a76a46496c8e224511ea2af8c8e7250ef083db
timetam <timetam24@naver.com>
2024-12-07 03:10:15 +0900

    Refactor : VulDB Main Content 4차 리팩토링

--- src/app/(posts)/vuldb/items/page.tsx
+++ src/app/(posts)/vuldb/items/page.tsx
@@ -2,11 +2,20 @@ import { auth } from "@/auth";
 import VulDBMainContent from "@/components/vulnerability-db/VulDBMainContent";
 import VulDBImageCardContainer from "@/components/vulnerability-db/VulDBImageCardContainer";
 import { BASE_URL } from "@/lib/const";
+import dynamic from "next/dynamic";
 
 async function fetchLatestPosts() {
   const res = await fetch(`${BASE_URL}/api/posts/latest`);
   return res.json();
 }
+
+const ToastContainer = dynamic(
+  () =>
+    import("@/components/vulnerability-db/Toast").then(
+      (mod) => mod.ToastContainer,
+    ),
+  { ssr: false },
+);
 
 export default async function VulDBPage() {
   const response = await fetchLatestPosts();
@@ -19,6 +28,7 @@ export default async function VulDBPage() {
     <div className="relative mx-auto mt-[1.688rem] flex min-h-[147rem] w-full max-w-[82.063rem] flex-col gap-[4.75rem] overflow-hidden px-[1rem]">
       <VulDBImageCardContainer posts={latestPosts} />
       <VulDBMainContent userId={userId} />
+      <ToastContainer />
     </div>
   );
 }
