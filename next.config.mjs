b7d00d79b3436441f6458c8351374dccea1190c5
timetam <timetam24@naver.com>
2024-12-10 05:44:35 +0900

    Chore : Next.js fetch 로깅 설정 추가 (fullUrl 활성화)

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@ import bundleAnalyzer from "@next/bundle-analyzer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   images: {
     remotePatterns: [
       {
