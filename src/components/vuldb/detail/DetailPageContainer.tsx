064f8de27834b87ec10b64d7f0450f71714a38ca
timetam <timetam24@naver.com>
2024-12-30 12:45:22 +0900

    Refactor : 데이터 페칭 중 에러 처리 코드 추가

--- src/components/vuldb/detail/DetailPageContainer.tsx
+++ src/components/vuldb/detail/DetailPageContainer.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import InfoMessage from "@/components/ui/InfoMessage";
 import DetailContent from "@/components/vuldb/detail/Content";
 import { fetchPostAndSimilarPosts } from "@/lib/api/posts";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import dynamic from "next/dynamic";
-import DetailHeader from "./Header";
 import DetailPageSkeleton from "./DetailPageSkeleton";
+import DetailHeader from "./Header";
 
 const SimilarPostList = dynamic(() => import("./SimilarPostList"));
 
@@ -16,7 +17,7 @@ export default function DetailPageContainer({
   postId: string;
   userId: number;
 }) {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["postAndSimilarPosts", postId, userId],
     queryFn: async () => await fetchPostAndSimilarPosts({ postId, userId }),
     staleTime: 60 * 60 * 1000,
@@ -27,14 +28,31 @@ export default function DetailPageContainer({
     return <DetailPageSkeleton />;
   }
 
-  if (data) {
-    const { post, latestPosts } = data;
+  if (isError) {
     return (
-      <>
-        <DetailHeader post={post} userId={userId} />
-        <DetailContent post={post} />
-        <SimilarPostList userId={userId} posts={latestPosts} />
-      </>
+      <InfoMessage
+        situation="게시글을 불러오는 중 오류가 발생했습니다."
+        solution="새로고침하거나 잠시 후 다시 시도해주세요. 문제가 계속되면 고객센터로 문의해주세요."
+      />
     );
   }
+
+  if (!data) {
+    return (
+      <InfoMessage
+        situation="게시글을 찾을 수 없습니다."
+        solution="다른 게시글을 선택해주세요."
+      />
+    );
+  }
+
+  const { post, latestPosts } = data;
+
+  return (
+    <>
+      <DetailHeader post={post} userId={userId} />
+      <DetailContent post={post} />
+      <SimilarPostList userId={userId} posts={latestPosts} />
+    </>
+  );
 }
