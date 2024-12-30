08ad8357ba92f1466944f430ee6f08804baa3942
timetam <timetam24@naver.com>
2024-12-30 11:56:37 +0900

    Fix : try catch문 추가

--- src/app/(posts)/vuldb/items/page.tsx
+++ src/app/(posts)/vuldb/items/page.tsx
@@ -1,24 +1,40 @@
 import { auth } from "@/auth";
-import VulDBMainContent from "@/components/vulnerability-db/VulDBMainContent";
+import ExceptionHandlingMessage from "@/components/vulnerability-db/ExceptionHandlingMessage";
 import VulDBImageCardContainer from "@/components/vulnerability-db/VulDBImageCardContainer";
+import VulDBMainContent from "@/components/vulnerability-db/VulDBMainContent";
 import { BASE_URL } from "@/lib/const";
 
 async function fetchLatestPosts() {
-  const res = await fetch(`${BASE_URL}/api/posts/latest`);
+  const res = await fetch(`${BASE_URL}/api/posts/latest`, {
+    cache: "force-cache",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch latest posts");
+  }
   return res.json();
 }
 
 export default async function VulDBPage() {
-  const response = await fetchLatestPosts();
-  const latestPosts = response.results || [];
+  try {
+    const response = await fetchLatestPosts();
+    const latestPosts = response.results || [];
 
-  const session = await auth();
-  const userId = session?.user?.userId || null;
+    const session = await auth();
+    const userId = session?.user?.userId || null;
 
-  return (
-    <div className="relative mx-auto mt-[1.688rem] flex min-h-[147rem] w-full max-w-[82.063rem] flex-col gap-[4.75rem] overflow-hidden px-[1rem]">
-      <VulDBImageCardContainer posts={latestPosts} />
-      <VulDBMainContent userId={userId} />
-    </div>
-  );
+    return (
+      <div className="relative mx-auto mt-[1.688rem] flex min-h-[147rem] w-full max-w-[82.063rem] flex-col gap-[4.75rem] overflow-hidden px-[1rem]">
+        <VulDBImageCardContainer posts={latestPosts} />
+        <VulDBMainContent userId={userId} />
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return (
+      <ExceptionHandlingMessage
+        situation="게시글을 불러오는 중 오류가 발생했습니다."
+        solution="새로고침하거나 잠시 후 다시 시도해주세요. 문제가 계속되면 고객센터로 문의해주세요."
+      />
+    );
+  }
 }
